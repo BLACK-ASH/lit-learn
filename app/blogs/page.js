f818eead2cf98136fdd77ac8e339b9ef874a9325
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-01-04 23:32:28 +0530

    Blog Hero Section Added

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,13 +1,11 @@
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import BlogHero from "@/components/myComponents/BlogHero"
 
 const page = () => {
   return (
     <div className="flex flex-col gap-6 items-center">
-      <h4 className="scroll-m-20 max-md:text-center text-xl font-semibold tracking-tight">This page is under development</h4>
-      <Link href="/">
-        <Button variant="default">Go Back Home</Button>
-      </Link>
+      <BlogHero />
     </div>
   )
 }
