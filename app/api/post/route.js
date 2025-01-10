d5bdb729a64f0075d61b81c78a6afea7425d1712
BLACK-ASH <100258345+BLACK-ASH@users.noreply.github.com>
2025-01-10 17:11:46 +0530

    2  Problem Fix

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -2,24 +2,28 @@ import connectDB from "@/lib/connectDB";
 import Blog from "@/lib/model/blog.model";
 
 export async function POST(req) {
-    const { title, description, content, author, category, rating } = await req.json();
-    const link = author + "-" + title.split(" ").join("-");
-
-    const existingBlog = await Blog.findOne({ link });
-
-    if (existingBlog) {
-        return new Response(JSON.stringify({ status: 400, message: "Blog with this title already exists" }), { status: 400 });
-    }
-
     try {
         await connectDB();
+        const { title, description, content, author, category, rating } = await req.json();
+        const link = author + "-" + title.split(" ").join("-");
+
+        const existingBlog = await Blog.findOne({ link });
+        if (existingBlog) {
+            return new Response(
+                JSON.stringify({ status: 400, message: "Blog with this title already exists" }),
+                { status: 400 }
+            );
+        }
+
         const blog = new Blog({ title, description, content, author, category, rating, link });
         await blog.save();
         return new Response(JSON.stringify(link), { status: 201 });
     } catch (error) {
-        return new Response(JSON.stringify(error), { status: 500 });
+        console.error("Error:", error);
+        return new Response(JSON.stringify({ status: 500, error: error.message }), { status: 500 });
     }
 }
+
 
 export async function GET(req) {
 
